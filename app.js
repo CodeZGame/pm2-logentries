0b7c52388ea14d753da310be2d0b8e89c34fc527
BenEko <benjamin.francoual@outlook.com>
2016-05-04 14:35:03 +0200

    log file

--- app.js
+++ app.js
@@ -9,8 +9,6 @@ var conf    = pmx.initModule();
 
 pm2.launchBus(function(err, bus) {
 	bus.on('*', function(event, data) {
-		if (event == 'process:event' && data.event == 'online') {
-			console.log('Process %s restarted %s', data.process.name, data.process.restart_time);
-		}
+		console.log('Process %s restarted %s', data.process.name, data.process.restart_time);
 	});
 });
